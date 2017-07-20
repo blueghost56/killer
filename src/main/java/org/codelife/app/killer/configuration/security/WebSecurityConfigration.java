87e638c22176b174785aa92a969b88f95bc88440
blueghost56 <alex.ching5@gmail.com>
2017-07-20 18:00:52 +0800

    web security config

---
+++ src/main/java/org/codelife/app/killer/configuration/security/WebSecurityConfigration.java
@@ -0,0 +1,10 @@
+package org.codelife.app.killer.configuration.security;
+
+/**
+ * web security config
+ *
+ * @author csl
+ * @create 07/20/2017 17:57
+ **/
+public class WebSecurityConfigration {
+}
