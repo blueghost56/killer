afdcd4e44dd61592478b9cdacf075a6619c2b5d1
blueghost56 <alex.ching5@gmail.com>
2017-07-20 18:18:31 +0800

    web security config

--- src/main/java/org/codelife/app/killer/configuration/security/WebSecurityConfigration.java
+++ src/main/java/org/codelife/app/killer/configuration/security/WebSecurityConfigration.java
@@ -1,10 +1,36 @@
 package org.codelife.app.killer.configuration.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 /**
  * web security config
  *
  * @author csl
  * @create 07/20/2017 17:57
  **/
-public class WebSecurityConfigration {
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/favicon.ico","/","/**/*.html","/**/*.css","/**/*.js")
+                .permitAll()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .anyRequest().authenticated();
+        http.headers().cacheControl();
+    }
 }
